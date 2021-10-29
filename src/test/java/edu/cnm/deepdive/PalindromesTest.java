c458e30af223fbdb13772d54eb886a692b409aa6
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-29 17:02:31 -0400

    Parameterized Tests for Palindromes.

--- src/test/java/edu/cnm/deepdive/PalindromesTest.java
+++ src/test/java/edu/cnm/deepdive/PalindromesTest.java
@@ -3,36 +3,14 @@ package edu.cnm.deepdive;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 class PalindromesTest {
 
-  final static String[] palindromes = {
-      "radar",
-      "Radar",
-      "A man, a plan, a canal - Panama!",
-      "A man, a plan, a dam - Hoover!",
-      "aBbA"
-  };
-
-  final static boolean[] expectedValues = {
-      true,
-      true,
-      true,
-      false,
-      true
-  };
-
-  @Test
-  void checkDenormalized() {
-    for (int i = 0; i < palindromes.length; i++) {
-      String inputValue = palindromes[i];
-      boolean expectedValue = expectedValues[i];
-      boolean actualValue = Palindromes.checkDenormalized(inputValue);
-      assertEquals(expectedValue, actualValue);
-    }
-  }
-
-  @Test
-  void checkRecursive() {
+  @ParameterizedTest
+  @CsvFileSource(resources = "palindromes.csv", numLinesToSkip = 1)
+  void checkRecursive(String input, boolean expected) {
+    assertEquals(expected, Palindromes.checkRecursive(input));
   }
 }
