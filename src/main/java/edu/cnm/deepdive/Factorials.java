491fef8c1eba73a36c020130c1e7778e5c34f013
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-31 23:28:44 -0400

    Changed Factorials to BigInteger Factorials.

--- src/main/java/edu/cnm/deepdive/Factorials.java
+++ src/main/java/edu/cnm/deepdive/Factorials.java
@@ -1,12 +1,19 @@
 package edu.cnm.deepdive;
 
+import java.math.BigInteger;
+
 public abstract class Factorials {
 
-  public static long computeRecursive(int n) throws IllegalArgumentException {
+  public static BigInteger computeRecursive(int n) throws IllegalArgumentException {
     if (n < 0) {
       throw new IllegalArgumentException("Parameter must be greater than or equal to zero.");
     }
-    return (n == 0) ? 1 : (n * computeRecursive(n-1));
+    if (n == 0) {
+      return new BigInteger("1");
+    } else {
+      return BigInteger.valueOf(n).multiply(computeRecursive(n-1));
+    }
+    //return (n == 0) ? 1 : (n.multiply(computeRecursive(n.subtract(1))));
   }
 
 }
--- src/test/java/edu/cnm/deepdive/FactorialsTest.java
+++ src/test/java/edu/cnm/deepdive/FactorialsTest.java
@@ -2,20 +2,21 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
 
 class FactorialsTest {
 
   //^\s*(\d+)\s*(\d+).*$ -> $1, ; $2,
-  final static int[] args = {0,1,5,10,13};
-  final static long[] expectedValues = {1,1,120,3628800,6227020800L};
+  final static int[] args = {0,1,5,10,13,32,41};
+  final static BigInteger[] expectedValues = {new BigInteger("1"),new BigInteger("1"),new BigInteger("120"),new BigInteger("3628800"),new BigInteger("6227020800"), new BigInteger("263130836933693530167218012160000000"), new BigInteger("33452526613163807108170062053440751665152000000000")};
 
   @Test
   void computeRecursive_nonNegative() {
     for (int i = 0; i < args.length; i++) {
       int n = args[i];
-      long expectedValue = expectedValues[i];
-      long actualValue = Factorials.computeRecursive(n);
+      BigInteger expectedValue = expectedValues[i];
+      BigInteger actualValue = Factorials.computeRecursive(n);
       assertEquals(expectedValue,actualValue);
     }
   }
